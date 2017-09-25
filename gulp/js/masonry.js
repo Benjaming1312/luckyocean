2eb8013c77cf76c08a71674dd7cac6c20cc583a0
Ben <ashitman1312@gmail.com>
2017-09-25 19:09:55 +0800

    首頁切版上線

--- gulp/js/masonry.js
+++ gulp/js/masonry.js
@@ -1,11 +1,17 @@
 $(function () {
-    $('.grid').each(function () {
-        $(this).masonry({
-            itemSelector: '.grid-item',
-            columnWidth: 100
-        })
-    })
     setTimeout(function () {
+        // $('.grid').each(function () {
+        //     $(this).masonry({
+        //         itemSelector: '.grid-item',
+        //         columnWidth: 80,
+        //         horizontalOrder: true,
+        //         fitWidth: true,
+        //         percentPosition: true,
+        //         columnWidth: '.grid-item',
+        //         itemSelector: '.grid-item',
+        //         gutter: 10,
+        //     })
+        // })
         $('.tab-content > div').addClass('tab-pane fade')
     },1000)
 })
