bdc460df0332774306dfd5c7ad8c4f95026fe53e
Ben <ashitman1312@gmail.com>
2018-02-08 02:34:43 +0800

    新增商品推薦模組改輪摸

--- gulp/js/owlCarousel.js
+++ gulp/js/owlCarousel.js
@@ -19,3 +19,47 @@
 //         $('.banner').append($('.banner >.hgroup'))
 //     }
 // })
+
+// 商品推薦模組改輪播
+// function owlMshop(target) {
+//   var owl = $(target).find('.module-special')
+//   owl.each(function () {
+//     if ($(this).find('.listBS').attr('class').indexOf('listSlide') < 0) {
+//       $(this).find('.mb').addClass('owl-carousel customSlide')
+//       $(this).find('.row.listBS').addClass('item')
+//     }
+//   })
+//   $('.customSlide').each(function () {
+//     $(this).owlCarousel({
+//       items: 4,
+//       nav: true,
+//       margin: 10,
+//       dots: false,
+//       loop: false,
+//       responsiveClass:true,
+//       responsive: {
+//         0:{
+//           items: 2,
+//           nav: true,
+//           margin: 10,
+//           dots: false,
+//           loop: false
+//         },
+//         600:{
+//           items: 2,
+//           nav: true,
+//           margin: 10,
+//           dots: false,
+//           loop: false
+//         },
+//         1000:{
+//           items: 4,
+//           nav: true,
+//           margin: 20,
+//           dots: false,
+//           loop: false
+//         }
+//       }
+//     })
+//   })
+// }
--- gulp/js/userjs.js
+++ gulp/js/userjs.js
@@ -1,6 +1,6 @@
 // $(function () {
 //     // owlcarouselfn('.banner .row .col-xs-12', false, true)
 // })
-$('test').click((e) => {
-    return e ? true : false
-})
+// $('test').click((e) => {
+//     return e ? true : false
+// })
